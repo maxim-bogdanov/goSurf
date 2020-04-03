571afc763934f28a5993e3b570e0881dc3a0625a
maxim-bogdanov <937933mb@mail.ru>
2020-04-03 22:09:16 +0300

    работа над картой

--- js/main.js
+++ js/main.js
@@ -5,11 +5,36 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.few-sliders',
   });
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
+    asNavFor: '.header__slider'
+  });
+  $('.header__slider-dates').slick({
+    infinite: true,
+    fade: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     asNavFor: '.header__slider',
+    centerPadding: '10px',
+    swipe: false
+  });
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    // variableWidth: true
+    asNavFor: '.slider-map'
+  });
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor:'.surf-slider',
+    focusOnSelect: true
   });
 });
